d6bef2b52d4fa3e3ba608d378aa65fea60305b7d
amiribnjaman <amiribnjaman@gmail.com>
2024-12-10 19:27:07 +0600

    Home page basic design implement

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="text-center mt-28 flex flex-col items-center justify-center">
+      <h1 className="text-[40px] font-semibold">
+        Japanese Language Learn Make easy with Jll
+      </h1>
+      <h5 className="text-[20px]">
+        We make leangeage vocabulary easy to learn and implentent.
+      </h5>
+      <button
+        type="button"
+        class="mt-8 focus:outline-none text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+      >
+        Explore
+      </button>
     </div>
   );
 }
