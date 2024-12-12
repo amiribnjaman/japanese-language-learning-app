b9b371c70e00ba3b4fb3d5e43bbd87b2544af647
amiribnjaman <amiribnjaman@gmail.com>
2024-12-12 12:38:26 +0600

    Auth route protection middlware issue resolv

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,36 @@
+// import { NextResponse } from "next/server";
+
+// export function middleware(request) {
+//   const authToken = request.cookies.get("Token")?.value;
+
+//   // const { pathname } = request.nextUrl;
+
+//   // Logedin user not access urls
+//   const logedInUserNotAccessUrl = ["/login", "/signup"]
+  
+
+//   // Logout user not access url
+//   const logoutUserNotAccessUrl = request.nextUrl.pathname === "/learn";
+
+//   // Logic to loged in user
+//   if (logedInUserNotAccessUrl) {
+//     if (authToken) {
+//       return NextResponse.redirect(new URL("/learn", request.url));
+//     }
+//   }
+
+//   // Logic for logout user
+//   if (!authToken && logoutUserNotAccessUrl) {
+//     return NextResponse.redirect(new URL("/login", request.url));
+//   }
+// }
+
+// export const config = {
+//   matcher: ["/", "/login", "/signup", "/learn"],
+// };
+
+
+
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
@@ -9,15 +42,16 @@ export function middleware(request) {
     request.nextUrl.pathname === "/signup";
 
   // Logout user not access url
-  const logoutUserNotAccessUrl = request.nextUrl.pathname === "/learn";
+  const logoutUserNotAccessUrl = request.nextUrl.pathname.includes("/learn");
+  console.log(logoutUserNotAccessUrl);
 
   // Logic for loged in user
   if (logedInUserNotAccessUrl) {
     if (authToken) {
-      return NextResponse.redirect(new URL("/", request.url));
+      return NextResponse.redirect(new URL("/learn", request.url));
     }
-    }
-    
+  }
+
   // Logic for logout in user
   if (logoutUserNotAccessUrl) {
     if (!authToken) {
@@ -27,6 +61,8 @@ export function middleware(request) {
 }
 
 
+
+
 export const config = {
-    matcher: ['/','/login','/signup', '/learn']
-}
+  matcher: ["/", "/login", "/signup", "/learn", "/learn/add-lession", "/learn/manage-user", "/tutorial", "/learn/", "/learn/add-vocabulary", "/learn/manage-lession","/learn/manage-vocabulary",],
+};
